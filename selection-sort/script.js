5d819080eea87ecd55898ed3d9c1e6e77fbf679d
rojii-sketch <kahindirodgers77@gmail.com>
2026-06-21 09:28:16 +0300

    Added selection sort algorithm

---
+++ selection-sort/script.js
@@ -0,0 +1,16 @@
+function selectionSort(array){
+  for(let i = 0; i < array.length; i++){
+    let minIndex = i
+    for (let j = i + 1; j < array.length; j++){
+      if(array[j] < array[minIndex]){
+        minIndex = j
+      }
+    }
+    if(minIndex !== i){
+      let temp = array[i]
+      array[i] = array[minIndex]
+      array[minIndex] = temp
+    }
+  }
+  return array
+}
